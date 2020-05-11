85db5f86104a6b922f485168ccf67f45f6b41082
changyou <changyou@qq.com>
2020-05-11 14:38:07 +0800

    123

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,7 +11,9 @@ Page({
       {"ip":"39.98.73.255","desc":"阿里云服务器","opt":"查看用户"},
       {"ip":"198.181.39.45","desc":"搬瓦工","opt":"查看用户"}
     ],
-    showAuthUser:false,
+    showAuthUser:false, //是否显示授权员工面板
+    addAuthUser:false,  //是否显示添加授权员工对话框
+    authorizationUserData:{}      //需要添加授权员工表单数据
   },
   
   Utils: new Utils(),
@@ -27,7 +29,7 @@ Page({
             that.setData({'show':true})
           } else {
             that.setData({'domain':res.data})
-            console.log(res)
+            //console.log(res)
             //请求接口获取服务资源数据
             dd.getAuthCode({
                 success:function(authRes){
@@ -140,17 +142,66 @@ Page({
     }
   },
 
-  //显示授权用户面板
+  //显示授权员工面板
   showAuthUsers(e){
       this.setData({"showAuthUser":true})
   },
   
-  //关闭授权用户面板
+  //关闭授权员工面板
   hiddenAuthUsers(e){
       this.setData({"showAuthUser":false})
   },
 
-  addAuthUser(e){
-    this.setData({"showAuthUser":false})
+  //显示添加授权员工面板
+  showAddAuthUser(e){
+    this.setData({"addAuthUser":true})
+  },
+
+  //隐藏添加授权员工面板
+  hideAddAuthUser(e){
+    this.setData({"addAuthUser":false})
+  },
+
+  //选择授权员工
+  chooseEmployee(e){
+    let that = this
+    dd.complexChoose({
+        title:"测试标题",            //标题
+        multiple:true,            //是否多选
+        limitTips:"超出了",          //超过限定人数返回提示
+        maxUsers:1000,            //最大可选人数
+        pickedUsers:[],            //已选用户
+        pickedDepartments:[],          //已选部门
+        disabledUsers:[],            //不可选用户
+        disabledDepartments:[],        //不可选部门
+        requiredUsers:[],            //必选用户（不可取消选中状态）
+        requiredDepartments:[],        //必选部门（不可取消选中状态）
+        permissionType:"xxx",          //可添加权限校验，选人权限，目前只有GLOBAL这个参数
+        responseUserOnly:false,        //返回人，或者返回人和部门
+        success:function(res){
+            if(res.users.length == 0){
+              dd.showToast({
+                type: 'fail',
+                content: '暂不支持部门选择',
+                duration: 2000
+              });
+            } else if(res.users.length > 1) {
+              dd.showToast({
+                type: 'fail',
+                content: '暂不支持多员工选择',
+                duration: 2000
+              });
+            } else {
+              let authorizationUserData = {}
+              authorizationUserData['employee_name'] = res.users[0]['name']
+              authorizationUserData['authorization_name'] = 'dingding-'+res.users[0]['userId']
+              authorizationUserData['authorization_pwd'] = 'dingding-'+res.users[0]['userId']
+              authorizationUserData['authorization_repwd'] = 'dingding-'+res.users[0]['userId']
+              that.setData({'authorizationUserData':authorizationUserData})
+            }
+        },
+        fail:function(err){
+        }
+    })
   }
 });
